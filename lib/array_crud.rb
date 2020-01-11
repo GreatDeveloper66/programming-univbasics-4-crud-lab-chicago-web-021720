a384199b0ea920eff7ebc5a6d0629072a9e5b472
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-11 21:40:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,27 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
@@ -35,5 +37,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
+  array
 end
